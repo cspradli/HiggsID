6d7816d8a9996f7083e3038505086d14d03161ad
cspradli <cspradli@unca.edu>
2019-12-02 12:45:35 -0500

    added comments and changed y limit in plot

--- dl_keras.py
+++ dl_keras.py
@@ -8,7 +8,7 @@ from sklearn.metrics import roc_curve, auc
 import matplotlib.pyplot as plt
 import os.path
 
-
+### Attempting to get rid of TF and Keras warnings about deprecation ###
 import warnings
 warnings.simplefilter("ignore")
 
@@ -53,7 +53,9 @@ numSpecs = len(specs)
 numLabels = len(labels)
 
 
-def get_feature_lables(fname, remove_mass_PTWINDOW=True):
+def get_feature_lables(fname, PTWINDOW=True):
+    """Sorts through the H5 file to find the feature array, specs array, and label array. Returns the feature array
+    in conjunction with the label array """
     file = tables.open_file(fname, 'r')
     numJets = getattr(file.root, features[0]).shape[0]
     feat_arr = np.zeros((numJets, numF))
@@ -74,7 +76,8 @@ def get_feature_lables(fname, remove_mass_PTWINDOW=True):
         fact1 = getattr(file.root, prod1)[:]
         lab_arr[:,i] = np.multiply(fact0, fact1)
     
-    if remove_mass_PTWINDOW:
+    if PTWINDOW:
+        ### checking to see if we can automatically throw out candidates if they are not within mass window ###
         feat_arr = feat_arr[(spec_arr[:,0] > 40)
                          & (spec_arr[:,0] < 200) 
                          & (spec_arr[:,1] > 300) 
@@ -90,12 +93,14 @@ def get_feature_lables(fname, remove_mass_PTWINDOW=True):
     file.close()
     return feat_arr, lab_arr
 
+### Using premade training data from CERN ###
 if not os.path.isfile('data/ntuple_merged_10.h5'):
     print("ERROR: data not found")
     exit(1)
 
-feat_arr, label_arr = get_feature_lables('data/ntuple_merged_10.h5', remove_mass_PTWINDOW=False)
+feat_arr, label_arr = get_feature_lables('data/ntuple_merged_10.h5', PTWINDOW=False)
 
+### Model being put together ###
 inputs = Input(shape=(numF,), name='Input')
 x = BatchNormalization(name='bn_1')(inputs)
 x = Dense(64, activation='relu')(x)
@@ -108,33 +113,36 @@ keras_mod = Model(inputs=inputs, outputs=outputs)
 keras_mod.compile(optimizer='adam', loss='categorical_crossentropy', metrics=['accuracy'])
 print(keras_mod.summary())
 
-
+### Check to make sure we aren't over training ###
 early_stopping = EarlyStopping(monitor='val_loss', patience=50)
 model_ch = ModelCheckpoint('models/keras_higgs_best.h5', monitor='val_loss', save_best_only=True)
 callb = [early_stopping, model_ch]
 
+### Fit the model ###
 keras_mod.fit(feat_arr, label_arr, batch_size=1024, epochs=100,
               validation_split=0.2, shuffle=True, callbacks=callb)
 
+
+########### Testing for fit model #############
+### using premade testing dataset from CERN ###
 if not os.path.isfile('ntuple_merged_0.h5'):
     print("ERROR: need testing data")
     print("Need ntuple_merged_0.h5")
     exit(0)
 
 feat_arr_test, label_arr_test = get_feature_lables('data/ntuple_merged_0.h5')
-
 keras_mod.load_weights('models/keras_higgs_best.h5')
-
 predict_arr_test = keras_mod.predict(feat_arr_test)
 
-fpr, tpr, threshold = roc_curve(label_arr_test[:,1], predict_arr_test[:,1])
 
+########### Plot ROC curve for AUC score #############
+fpr, tpr, threshold = roc_curve(label_arr_test[:,1], predict_arr_test[:,1])
 plt.figure()
 plt.plot(tpr, fpr, lw=2.5, label="AUC = {:.2f}%".format(auc(fpr,tpr)*100))
 plt.xlabel(r'True positive rate')
 plt.ylabel(r'False positive rate')
 plt.semilogy()
-plt.ylim(0.01,1)
+plt.ylim(0.001,1)
 plt.xlim(0,1)
 plt.grid(True)
 plt.legend(loc='upper left')
